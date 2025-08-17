881f402250b4c4b5de01a967b0d8ca3d3d7be6da
Linus-Daniel <vandulinus@gmail.com>
2025-08-17 07:58:41 +0100

    initial commit

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const Url = process.env.NODE_ENV === "production"?"https://conces-contest.vercel.app/api":"http://localhost:3000/api"
+const Url =
+  process.env.NODE_ENV === "production"
+    ? "https://conces-contest.vercel.app/app"
+    : "http://localhost:3000/api";
 
 const api = axios.create({
     baseURL: Url,
