4bda2050f09c37d559b35ddbb7502c0a289f442b
Linus-Daniel <vandulinus@gmail.com>
2025-10-22 07:00:45 +0100

    Full production

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    loader: "cloudinary",
-    unoptimized: true,
-    path: "https://res.cloudinary.com/dzr3vlosq/",
     domains: [
       "storage.googleapis.com",
       "images.unsplash.com",
